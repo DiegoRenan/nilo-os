6e830f3ccd2efddd1b9d943912a22dbdf36776df
DiegoRenan <renan.ccomp@gmail.com>
2019-03-11 23:09:51 -0300

    DEPARTMENTS: adiciona action create

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -2,4 +2,23 @@ class DepartmentsController < ApplicationController
   def new
   	@department = Department.new
   end
+
+  def create
+    @department = Department.new(department_params)
+    
+    if @department.save
+	    flash[:success] = "Departamento #{@department.name} criado."
+    	redirect_to @department
+	else
+       render 'new'
+    end
+  end
+
+  private
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def department_params
+      params.require(:department).permit(:name)
+    end
+
 end
