29a9ea2dc51ddb0e4a6da9ab6af0ac2eef773125
DiegoRenan <renan.ccomp@gmail.com>
2019-03-11 23:14:19 -0300

    DEPARTMENTS: show

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,11 +1,15 @@
 class DepartmentsController < ApplicationController
+  before_action :set_department, only: [:show, :edit, :update, :destroy]
   def new
   	@department = Department.new
   end
 
+  def show
+  end
+
   def create
     @department = Department.new(department_params)
-    
+
     if @department.save
 	    flash[:success] = "Departamento #{@department.name} criado."
     	redirect_to @department
@@ -15,7 +19,9 @@ class DepartmentsController < ApplicationController
   end
 
   private
-
+  	def set_department
+      @department = Department.find(params[:id])
+    end
     # Never trust parameters from the scary internet, only allow the white list through.
     def department_params
       params.require(:department).permit(:name)
