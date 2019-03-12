50302425417f65487d343cdeb03917840f5c5c09
DiegoRenan <renan.ccomp@gmail.com>
2019-03-11 22:51:45 -0300

    DEPARTMENTS: Cria controller department, action new e view new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do 	
+  get 'departments/new'
 	root 'users#index'
   	resources :users
+  	resources :departments
 
   	get    '/login',   to: 'sessions#new'
   	post   '/login',   to: 'sessions#create'
