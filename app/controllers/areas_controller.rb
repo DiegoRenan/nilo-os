6e1e111fca1850193c9b2eb5f4bcdbbde394f7b6
DiegoRenan <renan.ccomp@gmail.com>
2019-03-13 20:23:50 -0300

    AREA: create and show

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,5 +1,30 @@
 class AreasController < ApplicationController
+  before_action :set_area, only: [:show]
+
   def new
   	@area = Area.new
   end
+
+  def show
+  end
+
+  def create
+  	@area = Area.new(area_params)
+  	if @area.save
+  		flash[:success] = "Area #{@area.name} criada"
+  		redirect_to @area
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+  	def area_params
+  		params.require(:area).permit(:name)
+  	end
+
+  	def set_area
+  		@area = Area.find(params[:id])
+  	end
 end
