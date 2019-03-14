6722ce0badd0f0790733a0a1bcad95f342d985bb
DiegoRenan <renan.ccomp@gmail.com>
2019-03-13 21:47:47 -0300

    SERVICE_STATUS: create and show

--- app/controllers/service_statuses_controller.rb
+++ app/controllers/service_statuses_controller.rb
@@ -1,5 +1,31 @@
 class ServiceStatusesController < ApplicationController
+  before_action :set_service_status, only: [:show, :edit, :update, :destroy]
+
   def new
 	@service_status = ServiceStatus.new
   end
+
+  def show
+  end
+
+  def create
+  	@service_status = ServiceStatus.new(service_status_params)
+  	if @service_status.save
+  		flash[:success] = "Status #{@service_status.status} criado"
+  		redirect_to @service_status
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+  	def service_status_params
+  		params.require(:service_status).permit(:status)
+	end
+	
+  	def set_service_status
+  		@service_status = ServiceStatus.find(params[:id])
+  	end
+
 end
