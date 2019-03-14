cb50dc55bb44faf86417918a86374416c99f6035
DiegoRenan <renan.ccomp@gmail.com>
2019-03-13 22:01:00 -0300

    SERVICE_STATUS: index, edit, update e delete

--- app/controllers/service_statuses_controller.rb
+++ app/controllers/service_statuses_controller.rb
@@ -1,11 +1,18 @@
 class ServiceStatusesController < ApplicationController
   before_action :set_service_status, only: [:show, :edit, :update, :destroy]
 
+  def index
+  	@service_statuses = ServiceStatus.all
+  end
+
   def new
 	@service_status = ServiceStatus.new
   end
 
   def show
+  end
+
+  def edit
   end
 
   def create
@@ -18,12 +25,27 @@ class ServiceStatusesController < ApplicationController
   	end
   end
 
+  def update
+  	if @service_status.update(service_status_params)
+  		flash[:success] = "Status #{@service_status.status} atualizado"
+  		redirect_to @service_status
+  	else
+  		render 'new'
+  	end
+  end
+
+  def destroy
+  	@service_status.destroy
+  	flash[:success] = "Status removido"
+  	redirect_to service_statuses_path
+  end
+
   private
 
   	def service_status_params
   		params.require(:service_status).permit(:status)
 	end
-	
+
   	def set_service_status
   		@service_status = ServiceStatus.find(params[:id])
   	end
