1e25d2ac0621c292cf90561cbb8fd9c0c8aeb0b1
DiegoRenan <renan.ccomp@gmail.com>
2019-03-14 21:45:12 -0300

    SERVICES: show

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,4 +1,6 @@
 class ServicesController < ApplicationController
+  before_action :set_service, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user
   
   def index
 	    if current_user.admin?
@@ -11,6 +13,9 @@ class ServicesController < ApplicationController
   
   def new
   	@service = Service.new
+  end
+
+  def show
   end
 
   def create
@@ -29,5 +34,16 @@ class ServicesController < ApplicationController
   	def service_params
       params.require(:service).permit(:title, :body, :picture, :department_id, :sector_id, 
         :service_status_id, :service_type_id, :user_id)
+    end
+
+    def set_service
+      @service = Service.find(params[:id])
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Você não está logado."
+        redirect_to login_url
+      end
     end
 end
